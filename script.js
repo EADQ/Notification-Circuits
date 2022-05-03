be40b627643425af6e88d1f26b256c103a2a2cb3
EADQ <easoftwareengineer@gmail.com>
2022-05-02 21:07:00 -0600

    Little test with functions

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
-var getData = function(){
-    var name = document.getElementById("Sum_1").value;
-    var name = document.getElementById("Sum_2").value;
-    console.log(Sum_1+""+Sum_2);
+//var getData = function(){
+    //var name = document.getElementById("Sum_1").value;
+    //var name = document.getElementById("Sum_2").value;
+    //console.log(Sum_1+""+Sum_2);
 
-}
+//}
+
+
+function multiplicar(a, b){
+    var result=(a*b);
+    console.log(result), 
+};
+
+multiplicar(10, 10)
+console.log(result);
